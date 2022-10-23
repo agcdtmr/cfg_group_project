b8089cf8817eea6426198bf0d85b06b1ee4b8af6
HsShortland <98280528+HsShortland@users.noreply.github.com>
2022-10-23 17:02:19 +0100

    Updated to include columns based on data pulled from API

--- Create_job_search_database.sql
+++ Create_job_search_database.sql
@@ -4,6 +4,6 @@ CREATE TABLE users(ID INT NOT NULL UNIQUE PRIMARY KEY, surname VARCHAR(50), firs
  email VARCHAR(100) UNIQUE);
 CREATE TABLE login_details(username VARCHAR(100) UNIQUE PRIMARY KEY, _password VARCHAR(100),
  ID INT NOT NULL UNIQUE, CONSTRAINT FK_category FOREIGN KEY(ID) REFERENCES users(ID));
- CREATE TABLE saved_jobs(job_title VARCHAR(100) NOT NULL PRIMARY KEY, company VARCHAR(100),
- salary£ INT, location VARCHAR(100), applied_for_job VARCHAR(3), ID INT NOT NULL UNIQUE,
- CONSTRAINT FK_category2 FOREIGN KEY(ID) REFERENCES users(ID));
+ CREATE TABLE saved_jobs(employerID INT, employerName VARCHAR(100), expirationDate DATE, jobDescription VARCHAR(5000), jobID INT,
+ jobTitle VARCHAR(100) NOT NULL PRIMARY KEY, jobURL VARCHAR(1000), locationName VARCHAR(100), maximumSalary INT, minimumSalary INT,
+ applied_for_job VARCHAR(3), ID INT NOT NULL UNIQUE, CONSTRAINT FK_category2 FOREIGN KEY(ID) REFERENCES users(ID));
