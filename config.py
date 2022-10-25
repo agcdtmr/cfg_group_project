e713afa09e9c2ab7254062d06f251c1a255946ec
Cri-P <112575558+Cri-P@users.noreply.github.com>
2022-10-25 15:59:43 +0100

    job_search_engine
    Adding the most recent version

---
+++ config.py
@@ -0,0 +1,8 @@
+#Secret Key for Flask session
+SECRET_KEY = "secret-key"
+
+#Database connection information
+HOST='localhost'
+DATABASE='job_search1' ###alternative DB to "job_search"
+USER="root"
+PASSWORD="CriCri333Python"
