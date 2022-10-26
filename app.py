5e6d64336ed8b0f376afc11a3d577e5e1dad5509
Cri-P <112575558+Cri-P@users.noreply.github.com>
2022-10-26 21:56:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+#creating the server for the job engine application
+
+from flask import Flask
+from views import views
+from config import SECRET_KEY
+
+
+
+app= Flask(__name__)
+app.register_blueprint(views, url_prefix='/')
+
+app.secret_key = SECRET_KEY
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
