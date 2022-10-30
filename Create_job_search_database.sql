7921a1884e620f51d9d848f8fa493aeac5dd6268
HsShortland <98280528+HsShortland@users.noreply.github.com>
2022-10-30 18:32:53 +0000

    Updated to be inline with the add_user function, removed not null for time being to enable Python functionality and added a unique ID column to saved_jobs table.

--- Create_job_search_database.sql
+++ Create_job_search_database.sql
@@ -1,9 +1,12 @@
 CREATE DATABASE job_search;
+
 USE job_search;
-CREATE TABLE users(ID INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, surname VARCHAR(50), first_name VARCHAR(50),
- email VARCHAR(100) UNIQUE);
-CREATE TABLE login_details(username VARCHAR(100) UNIQUE PRIMARY KEY, _password VARCHAR(100),
- ID INT NOT NULL UNIQUE AUTO_INCREMENT, CONSTRAINT FK_category FOREIGN KEY(ID) REFERENCES users(ID));
- CREATE TABLE saved_jobs(employerID INT, employerName VARCHAR(100), expirationDate DATE, jobDescription VARCHAR(5000), jobID INT,
- jobTitle VARCHAR(100) NOT NULL PRIMARY KEY, jobURL VARCHAR(1000), locationName VARCHAR(100), maximumSalary INT, minimumSalary INT,
- applied_for_job VARCHAR(3), ID INT NOT NULL UNIQUE, CONSTRAINT FK_category2 FOREIGN KEY(ID) REFERENCES users(ID));
+
+CREATE TABLE users(user_ID INT UNIQUE PRIMARY KEY AUTO_INCREMENT, surname VARCHAR(50), first_name VARCHAR(50),
+username VARCHAR(100) UNIQUE, email VARCHAR(100) UNIQUE, password VARCHAR(100));
+
+
+ CREATE TABLE saved_jobs(saved_job_ID INT UNIQUE PRIMARY KEY AUTO_INCREMENT, user_id INT UNIQUE, employerID INT, employerName VARCHAR(100),
+ expirationDate DATE, jobDescription VARCHAR(5000), jobID INT, jobTitle VARCHAR(100), jobURL VARCHAR(1000),
+ locationName VARCHAR(100), maximumSalary INT, minimumSalary INT, applied_for_job VARCHAR(3),
+ FOREIGN KEY(user_ID) REFERENCES users(user_ID));
