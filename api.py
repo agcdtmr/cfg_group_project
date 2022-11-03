f2b009e3f3f01ea7fde521cf66a9d7059e3091c6
HsShortland <98280528+HsShortland@users.noreply.github.com>
2022-11-03 12:18:30 +0000

    deleted unnecessary files jobs-title-search.html and jobs-title-results.html, re-labelled the jobsearch filter button to match the jobsearch.html file

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ from requests.auth import HTTPBasicAuth
 from flask import jsonify
 import requests
 
-
 def get_from_api():
     # url = "https://xxxx"
     # API_KEY = "HTTPBasicAuth('d71bf436-fc9f-47fb-9a1f-2035ae09c27f"
@@ -12,15 +11,15 @@ def get_from_api():
     response = requests.get('https://www.reed.co.uk/api/1.0/search?keywords=programmer,tech,frontend%20developer,'
                             'backend%20developer,devops,software%20engineer,junior',
                auth=HTTPBasicAuth('d71bf436-fc9f-47fb-9a1f-2035ae09c27f', '')).json()
-               
-    data = [{
-        "city": entry['locationName'],
-        "job_title": entry['jobTitle'],
-        "jobId": entry['jobId'],
-        "employer_name": entry['employerName'],
-        "link": entry['jobUrl'],
-        "description": entry['jobDescription'],
-        "exp_date": entry['expirationDate']
+    for job in response['results']:
+        data = [{
+            "city": entry['locationName'],
+            "job_title": entry['jobTitle'],
+            "jobId": entry['jobId'],
+            "employer_name": entry['employerName'],
+            "link": entry['jobUrl'],
+            "description": entry['jobDescription'],
+            "exp_date": entry['expirationDate']
     } for entry in response['results']]
 
     return data
