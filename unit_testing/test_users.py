d26a3c100584bc7a996f185b55a44c27c0143739
anjdtmr <anjdeitmer@gmail.com>
2022-11-04 18:52:52 +0100

    Update unit testing

---
+++ unit_testing/test_users.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from cfg_group_project.Database.users import add_user, email_available, get_user_by_credentials, get_user_by_id
+import cfg_group_project.Database.connection
+
+
+class TestUsers(TestCase):
+
+    def test_add_user(self):
+        pass
+
+    def test_email_available(self):
+        pass
+
+    def test_get_user_by_credentials(self):
+        pass
+
+    def test_get_user_by_id(self):
+        pass
---
+++ unit_testing/test_views.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from cfg_group_project.views import view_home, submit_login, submit_signup
+from flask import Flask
+
+
+class TestViews(TestCase):
+
+    def test_view_home(self):
+        pass
+
+    def test_job_engine(self):
+        pass
+
+    def test_job_search(self):
+        pass
+
+    def test_jobsearch(self):
+        pass
+
+    def test_job_search_result(self):
+        pass
+
+    def test_view_login(self):
+        pass
+
+    def test_submit_login(self):
+        pass
+
+    def test_view_signup(self):
+        pass
+
+    def test_submit_signup(self):
+        pass
+
+    def test_submit_logout(self):
+        pass
+
+    def view_profile(self):
+        pass
